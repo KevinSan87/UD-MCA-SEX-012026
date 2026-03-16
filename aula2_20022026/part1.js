42ddf58826631551f6ce23a7ca5340f8a4b10cc3
Júlia Barbuglio <142731784+juliabarbuglio@users.noreply.github.com>
2026-03-16 11:37:01 -0300

    Add fruit array and log operations

--- aula2_20022026/part1.js
+++ aula2_20022026/part1.js
@@ -1,1 +1,25 @@
+let frutas = [
+'Maçã','Banana','Laranja','Uva','Pera','Manga','Abacaxi','Melancia',
+'Morango','Kiwi','Cereja','Limão','Coco','Mamão','Acerola','Goiaba',
+'Caqui','Pitanga','Framboesa','Amora','Maracujá'
+];
 
+console.log("Posição 0:", frutas[0]);
+console.log("Posição 7:", frutas[7]);
+console.log("Posição 11:", frutas[11]);
+console.log("Posição 15:", frutas[15]);
+console.log("Posição 18:", frutas[18]);
+console.log("Posição 20:", frutas[20]);
+
+
+console.log("Penúltima:", frutas[frutas.length - 2]);
+console.log("Última:", frutas[frutas.length - 1]);
+
+
+console.log("Total de elementos:", frutas.length);
+
+frutas.push("Graviola");
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
