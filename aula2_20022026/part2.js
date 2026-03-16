a0b6c31ec9ed2b880330c589b4ece330df3a8a73
Júlia Barbuglio <142731784+juliabarbuglio@users.noreply.github.com>
2026-03-16 11:37:40 -0300

    Define product object and log its attributes
    Initialize product object with properties and log details.

--- aula2_20022026/part2.js
+++ aula2_20022026/part2.js
@@ -1,1 +1,17 @@
+let produto = {
+nome: 'Camiseta',
+cor: 'Preta',
+preco: 59.90,
+estoque: 50
+};
 
+console.log(produto.nome);
+
+console.log(produto["preco"]);
+
+produto.estoque = 80;
+
+console.log("Nome:", produto.nome);
+console.log("Cor:", produto.cor);
+console.log("Preço:", produto.preco);
+console.log("Estoque:", produto.estoque);
